92cd7e5d686078a6405528bc104a78bc522ea41f
Akshith06 <64221592+Akshith06@users.noreply.github.com>
2020-10-22 20:20:47 -0500

    Add files via upload

--- p5.play-boilerplate-master (1)/sketch.js
+++ p5.play-boilerplate-master (1)/sketch.js
@@ -16,7 +16,7 @@ function draw() {
   if(wall.x-car.x < (car.width+wall.width)/2)
   {  
   car.velocityX=0;
-  var deformation=0.5*weight*speed*speed/2250;
+  deformation = 0.5*weight*speed*speed/2250;
 
   if(deformation<100)
   {
@@ -30,6 +30,7 @@ function draw() {
   {
       car.shapeColor=color(255,0,0);
   }
-  drawSprites();
 }
+  drawSprites();
+
 }
